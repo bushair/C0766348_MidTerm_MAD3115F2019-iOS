56287cd0af84fb50b0310a2725b69146aa53578b
bushair <bushairkm007@gmail.com>
2019-10-31 21:29:55 -0400

    validate Function

--- C0766348_MidTerm_MAD3115F2019-iOS/LogInViewController.swift
+++ C0766348_MidTerm_MAD3115F2019-iOS/LogInViewController.swift
@@ -27,32 +27,7 @@ class LogInViewController: UIViewController
     }
     func validateUser()
     {
-        if(txtuserName.text == "bushair@gmail.com" && txtPassword.text == "bushair123")
-        {
-            let myStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "CustomerVC") as! CustomerListTableViewController
-            self.present(nextVC, animated: true, completion: nil)
-            
-        }
-        else
-        {
-             //Create AlertController Object
-            let alert  =
-                UIAlertController(title: "Error", message: "User Email / Password incorrect", preferredStyle: UIAlertController.Style.alert)
-            //UIAlertController.Style.alert
-            
-            //Step - 2
-            //OK Button add
-            let actionOk = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-            
-            //Step - 3
-            //Add Button to Alert
-            alert.addAction(actionOk)
-            
-            //Step - 4
-            //Present Alert Dialog to the user
-            self.present(alert, animated: true, completion: nil)
-        }
+       
     }
     
 
