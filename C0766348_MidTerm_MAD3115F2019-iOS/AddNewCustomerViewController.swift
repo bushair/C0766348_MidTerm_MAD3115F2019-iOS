41cd29196e6a734d95135fac9ac5f45e6333a759
Bushair <bushairkm007@gmail.com>
2019-11-08 15:08:49 -0500

    Add  new customer class

---
+++ C0766348_MidTerm_MAD3115F2019-iOS/AddNewCustomerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddNewCustomerViewController.swift
+//  Project
+//
+//  Created by vamsi on 06/11/19.
+//  Copyright © 2019 vamsi. All rights reserved.
+//
+
+import UIKit
+
+protocol AddNewCustomerViewControllerDelegate:NSObject {
+    func didSelectSaveBtn(_ name:String)
+}
+
+class AddNewCustomerViewController: UIViewController {
+    
+        }
+    }
+}
